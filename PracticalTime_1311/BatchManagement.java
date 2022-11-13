a476e1b9e581a9840f15ad71dade38eaed292359
HoangAnh <anhitachi17@gmail.com>
2022-11-13 14:34:50 +0700

    not done BatchManagement and Main

---
+++ PracticalTime_1311/BatchManagement.java
@@ -0,0 +1,12 @@
+package PracticalTime_1311;
+
+public class BatchManagement {
+	public void createBatch() {
+		
+	}
+
+	public void display() {
+
+	}
+
+}
---
+++ PracticalTime_1311/Main.java
@@ -0,0 +1,9 @@
+package PracticalTime_1311;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
