cee6e19433f41f2230c2e3d9c064ee9deb412215
Tien hoc lap trinh <doantien2001@gmail.com>
2022-11-13 14:32:55 +0700

    done

---
+++ Candidate.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Candidate extends Person {
+
+    private double gpa;
+
+    public Candidate(int id, String candiateName, String gender, String email) {
+        super(id, candiateName, gender, email);
+        this.gpa = gpa;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+}
---
+++ Interviewer.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Interviewer extends Person {
+
+    private int experience;
+
+    public Interviewer(int experience, int id, String candiateName, String gender, String email) {
+        super(id, candiateName, gender, email);
+        this.experience = experience;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+}
